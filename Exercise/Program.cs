bc82e1e4c08a0beaee35dd3cad4587dc6e86d493
Roman <roman.panchenko.1999@mail.ru>
2023-04-10 21:23:40 +0600

    Added solution of task

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[5] {"fox", "go", "create", "burn", "001"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++){
+    if(array1[i].Length <= 3){
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array){
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+System.Console.WriteLine("Из имеющегося массива строк сформируем новый массив, изымая из первого значения с <=3 символами");
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
